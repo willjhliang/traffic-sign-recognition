ce1aa3031f6dc9c6eeea9731657f7d03fdcfc4c7
Will Liang <willjhliang@gmail.com>
2022-12-07 17:30:37 -0500

    split data into train and test

--- clean_data.py
+++ clean_data.py
@@ -6,6 +6,7 @@ import csv
 
 
 K = 58
+test_ratio = 0.1
 
 def clean_data(datapath):
     """Loads images from files and performs basic pre-processing."""
@@ -20,6 +21,8 @@ def clean_data(datapath):
         classFreq.append(0)
 
     os.mkdir('data/filtered_images')
+    os.mkdir('data/filtered_images/train')
+    os.mkdir('data/filtered_images/test')
 
     for f in os.listdir(datapath):
         k = int(f[:3])  # Get label from filename
@@ -34,15 +37,15 @@ def clean_data(datapath):
             continue
         for i, img in enumerate(data[k]):
             img = Image.fromarray(img)
-            img.save(f"data/filtered_images/{kp:03}_{i:03}.png")
+            if i < len(data[k]) * test_ratio:
+                img.save(f"data/filtered_images/test/{kp:03}_{i:03}.png")
+            else:
+                img.save(f"data/filtered_images/train/{kp:03}_{i:03}.png")
         kp += 1
         new_labels.append(labels[k])
 
-
     with open('data/filtered_labels.csv', 'w') as f2:
-
         writer = csv.writer(f2)
-
         writer.writerow(['ClassId', 'Name'])
         for i in range(len(new_labels)):
             towrite = [str(i), new_labels[i]['Name']]
