1c430aa8fe740466377b8859e8590a34aa7b2248
Will Liang <willjhliang@gmail.com>
2022-12-09 17:35:03 -0500

    fixed torch eval probably

--- torch_utils.py
+++ torch_utils.py
@@ -1,12 +1,15 @@
 
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+
 import torch
 from torch import nn
 from torch import optim
 from torch.utils import data
 from torch import Tensor
-from matplotlib import pyplot as plt
-from tqdm import tqdm
+from torchmetrics import Accuracy, Precision, Recall, F1Score
 
+from constants import K
 from data import split_validation
 
 
@@ -27,9 +30,11 @@ def train_model(model, train_loader, val_loader, epochs, lr):
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr) 
 
-    train_loss = []
-    train_acc = []
-    val_acc = []
+    losses = []
+    train_accuracy, val_accuracy = [], []
+    train_precision, val_precision = [], []
+    train_recall, val_recall = [], []
+    train_f1, val_f1 = [], []
     for epoch in range(epochs):
         running_loss = 0
         for itr, (image, label) in tqdm(enumerate(train_loader), total=len(train_loader), leave=False):
@@ -42,26 +47,45 @@ def train_model(model, train_loader, val_loader, epochs, lr):
             loss.backward()
             optimizer.step()
     
-        train_loss.append(running_loss)
-        train_acc.append(evaluate_model(model, train_loader)[-1])
-        val_acc.append(evaluate_model(model, val_loader)[-1])
-        print(f'Epoch: {epoch+1:03}, Loss: {running_loss:9.4f}, Train Accuracy: {train_acc[-1]:.4f}, Validation Accuracy: {val_acc[-1]:.4f}')
+        losses.append(running_loss)
+        _, (accuracy, precision, recall, f1) = evaluate_model(model, train_loader)
+        train_accuracy.append(accuracy)
+        train_precision.append(precision)
+        train_recall.append(recall)
+        train_f1.append(f1)
+        _, (accuracy, precision, recall, f1) = evaluate_model(model, val_loader)
+        val_accuracy.append(accuracy)
+        val_precision.append(precision)
+        val_recall.append(recall)
+        val_f1.append(f1)
+        print(f'Epoch: {epoch+1:03}, Loss: {running_loss:9.4f}, Train F1: {train_f1[-1]:.4f}, Validation F1: {val_f1[-1]:.4f}')
 
-    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
-    axs[0].plot(train_loss)
-    axs[1].plot(list(range(epochs)), train_acc, val_acc);
+    fig, axs = plt.subplots(1, 5, figsize=(20, 2.5))
+    axs[0].plot(losses)
+    axs[1].plot(list(range(epochs)), train_accuracy, val_accuracy);
+    axs[2].plot(list(range(epochs)), train_precision, val_precision);
+    axs[3].plot(list(range(epochs)), train_recall, val_recall);
+    axs[4].plot(list(range(epochs)), train_f1, val_f1);
 
     return model
 
 
 def evaluate_model(model, dataloader):
-    """Evaluates model and returns predictions and accuracy."""
-    y_pred = []
-    y_pred = torch.Tensor()
-    with torch.no_grad():
-        for _, (image, _) in enumerate(dataloader):
-            outputs = model(image)
-            _, predicted = torch.max(outputs.data, 1)
-            y_pred = torch.cat((y_pred, predicted))
+    """Evaluates model and returns predictions and metrics."""
+    accuracy = Accuracy(task='multiclass', num_classes=K)
+    precision = Precision(task='multiclass', num_classes=K)
+    recall = Recall(task='multiclass', num_classes=K)
+    f1 = F1Score(task='multiclass', num_classes=K)
 
-    return y_pred.tolist()
+    pred = torch.Tensor([])
+    with torch.no_grad():
+        for _, (images, labels) in enumerate(dataloader):
+            outputs = model(images)
+            _, batch_pred = torch.max(outputs.data, 1)
+            pred = torch.cat((pred, batch_pred))
+            accuracy(batch_pred, labels)
+            precision(batch_pred, labels)
+            recall(batch_pred, labels)
+            f1(batch_pred, labels)
+
+    return pred.tolist(), (accuracy.compute(), precision.compute(), recall.compute(), f1.compute())
